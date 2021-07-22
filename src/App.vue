e8bf694886f2a1acfd74a60a1428e6ebf4f20ac9
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-07-22 18:24:05 -0300

    - Added authentication w/ cognito

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
+<amplify-auth-container>
+  <amplify-authenticator>
+
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link>
   </div>
   <router-view />
+
+    <amplify-sign-out></amplify-sign-out>
+  </amplify-authenticator>
+</amplify-auth-container>
+
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,27 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import Amplify, { Auth } from 'aws-amplify';
+
+import { 
+    applyPolyfills,
+    defineCustomElements
+  } from '@aws-amplify/ui-components/loader';
+  
+
+Amplify.configure({
+    Auth: {
+        region: '',
+        userPoolId: '',
+        userPoolWebClientId: '',
+    }
+});
+
+applyPolyfills().then(() => {
+    defineCustomElements(window);
+  });
+  
+const app = createApp(App)
+
+app.config.isCustomElement = tag => tag.startsWith('amplify-');
+app.use(router).mount('#app')
