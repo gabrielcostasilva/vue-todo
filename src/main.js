a9f31a5454ef32a044cb567dd98e342d5b8a76ef
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-11-15 18:23:51 -0300

    Updated Amplify dependencies

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import Amplify, { Auth } from 'aws-amplify';
-
-import { 
-    applyPolyfills,
-    defineCustomElements
-  } from '@aws-amplify/ui-components/loader';
-  
+import AmplifyVue from '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue/styles.css';
 
 Amplify.configure({
     Auth: {
@@ -17,12 +13,6 @@ Amplify.configure({
         userPoolWebClientId: '',
     }
 });
-
-applyPolyfills().then(() => {
-    defineCustomElements(window);
-  });
   
 const app = createApp(App)
-
-app.config.isCustomElement = tag => tag.startsWith('amplify-');
-app.use(router).mount('#app')
+app.use(router).use(AmplifyVue).mount('#app')
